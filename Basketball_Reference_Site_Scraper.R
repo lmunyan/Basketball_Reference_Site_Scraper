f526509948f19e793fe28908e6cf4fec67e9073f
Luke Munyan <23621795+lmunyan@users.noreply.github.com>
2018-12-28 12:18:08 -0500

    Update Basketball_Reference_Site_Scraper.R

--- Basketball_Reference_Site_Scraper.R
+++ Basketball_Reference_Site_Scraper.R
@@ -13,27 +13,25 @@ NBA_Leaders <- function(year, metric){
   
   #loop for scraping basketball reference and binding all years together
   for(i in 1: length(year)){
-  
-  URL<-paste0("https://www.basketball-reference.com/leagues/NBA_", year[i], "_", metric, ".html")
-  
-  #scrape site using rvest, and chain with dplyr 
-  test<- URL %>%
-    read_html() %>%
-    html_nodes("table") %>%
-    html_table() %>%
-    as.data.frame() %>%
-    mutate(Year = year[i])
-  
-  combined <- rbind(combined, test)
+    
+    URL<-paste0("https://www.basketball-reference.com/leagues/NBA_", year[i], "_", metric, ".html")
+    
+    #scrape site using rvest, and chain with dplyr 
+    test<- URL %>%
+      read_html() %>%
+      html_nodes("table") %>%
+      html_table() %>%
+      as.data.frame() %>%
+      mutate(Year = year[i])
+    
+    combined <- rbind(combined, test)
   }
   #write output to a csv file
-  fwrite(combined, paste0("NBA_", year[1], "_", year[length(year)], "_", metric, ".csv"))
   
   #write the data frame to global enviroment for further manipulation
-  combined <<- combined
+  return(combined)
 }
 
 #watch the function in action
-
 #metric options: per_game, totals, per_minute, per_poss, advanced, ratings
-NBA_Leaders(year= 2012:2016, metric = "per_minute")
+data <- NBA_Leaders(year= 2012:2016, metric = "per_minute")
